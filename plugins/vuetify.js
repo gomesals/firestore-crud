c76b1d07f613ade8c2ed15ef50829ec9b54f562b
Alexandre Silva <contato@silvalexandre.com>
2018-05-01 14:10:33 -0300

    Change theme

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.orange.darken1,
+    accent: colors.lightBlue.darken3
+  }
+})
